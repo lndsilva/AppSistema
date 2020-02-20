a8081fbb237d0c41d7e420a9b777db1b7520132b
manager <lndsilva@hotmail.com>
2020-02-19 21:04:31 -0300

    sistema app

---
+++ app/src/main/java/br/com/etecia/appsistema/MainActivity.java
@@ -0,0 +1,14 @@
+package br.com.etecia.appsistema;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/br/com/etecia/appsistema/Principal_Activity.java
@@ -0,0 +1,14 @@
+package br.com.etecia.appsistema;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Principal_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.principal_layout);
+    }
+}
